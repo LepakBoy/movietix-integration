32ec0282030729926ddfb27f7e20c60dd025166a
LepakBoy <fikrinadzif35@gmail.com>
2021-12-17 16:15:37 +0700

    add pagination schedule movie

--- src/components/MovieDetail/schedule.jsx
+++ src/components/MovieDetail/schedule.jsx
@@ -8,23 +8,32 @@ import locationPicker from "../../assets/logo/location.png";
 import ebv from "../../assets/logo/ebv.png";
 import hiflix from "../../assets/logo/hiflix.png";
 import cineone from "../../assets/logo/cineone.png";
+import Pagination from "react-paginate";
 
 export default function MovieDetailSchedule(props) {
   const [schedules, setSchedules] = useState([]);
   const [cities, setCities] = useState(["", "jakarta", "bandung", "bogor", "depok"]);
   const [selectedCity, setSelectedCity] = useState("");
+  const [paginate, setPaginate] = useState({ page: 1, limit: 2, totalPage: 1 });
+  const [selectedTime, setSelectedTime] = useState({ time: "", id_schedule: "" });
 
-  const getSchedule = () => {
+  const getSchedule = (pageSelect) => {
+    const { limit } = paginate;
     axios
       .get(
-        `/schedule/all?page=1&limit=10&location=${selectedCity}&sort=DESC&movie_id=${props.idMovie}`
+        `/schedule/all?page=${pageSelect}&limit=${limit}&location=${selectedCity}&sort=DESC&movie_id=${props.idMovie}`
       )
       .then((res) => {
         setSchedules(res.data.data);
+        setPaginate({ ...paginate, totalPage: res.data.pagination.totalPage });
       });
   };
 
-  console.log(selectedCity);
+  const handlePagination = (e) => {
+    const selectedPage = e.selected + 1;
+    setPaginate({ ...paginate, page: selectedPage });
+    getSchedule(selectedPage);
+  };
 
   useEffect(() => {
     getSchedule();
@@ -62,9 +71,7 @@ export default function MovieDetailSchedule(props) {
     I */}
                     {cities.map((item) => (
                       <li key={item} onClick={() => setSelectedCity(item)}>
-                        <a className="dropdown-item" href="#">
-                          {item === "" ? "All locations" : item}
-                        </a>
+                        <a className="dropdown-item">{item === "" ? "All locations" : item}</a>
                       </li>
                     ))}
                   </ul>
@@ -76,61 +83,85 @@ export default function MovieDetailSchedule(props) {
             <div className="row d-flex justify-content-evenly text-center">
               {schedules.length > 0 ? (
                 schedules.map((item) => (
-                  <div className="schedule-1 schedule-list p-4 mx-2" key={item.id_schedule}>
-                    <div className="header d-flex justify-content-md-between">
-                      <div className="logo pe-4">
-                        <img
-                          src={
-                            item.teater_name === "ebu.id"
-                              ? ebv
-                              : item.teater_name === "hiflix"
-                              ? hiflix
-                              : cineone
-                          }
-                          alt="logo"
-                        />
-                      </div>
-                      <div className="cinema-detail">
-                        <div className="cinema-detail__name text-start pb-1">
-                          {item.teater_name}
+                  <>
+                    <div className="schedule-1 schedule-list p-4 mx-2" key={item.id_schedule}>
+                      <div className="header d-flex justify-content-md-between">
+                        <div className="logo pe-4">
+                          <img
+                            src={
+                              item.teater_name === "ebu.id"
+                                ? ebv
+                                : item.teater_name === "hiflix"
+                                ? hiflix
+                                : cineone
+                            }
+                            alt="logo"
+                          />
                         </div>
-                        <div className="cinema-detail__address text-start">
-                          {/* Whatever street No.12, South Purwokerto */}
-                          {item.location}
+                        <div className="cinema-detail">
+                          <div className="cinema-detail__name text-start pb-1">
+                            {item.teater_name}
+                          </div>
+                          <div className="cinema-detail__address text-start">
+                            {/* Whatever street No.12, South Purwokerto */}
+                            {item.location}
+                          </div>
                         </div>
                       </div>
+                      <div className="time-schedule-list d-flex justify-content-sm-start flex-wrap pt-4">
+                        {item.time_schedule.map((time, index) => (
+                          <span
+                            className={
+                              item.id_schedule === selectedTime.idSchedule &&
+                              time === selectedTime.time
+                                ? "text-danger"
+                                : null
+                            }
+                            key={time}
+                            onClick={() => {
+                              props.handleSchedule(
+                                time,
+                                item.id_schedule,
+                                item.price,
+                                item.teater_name
+                              );
+                              setSelectedTime({ time: time, idSchedule: item.id_schedule });
+                            }}
+                          >
+                            {time}
+                          </span>
+                        ))}
+                      </div>
+                      <div className="schedule-price d-flex justify-content-between pt-4">
+                        <div className="price-title">Price</div>
+                        <div className="price-amount">{item.price} / seat</div>
+                      </div>
+                      <div className="booking-button pt-4">
+                        <button className="btn-booking" onClick={props.order}>
+                          Book now
+                        </button>
+                      </div>
                     </div>
-                    <div className="time-schedule-list d-flex justify-content-sm-start flex-wrap pt-4">
-                      {item.time_schedule.map((time) => (
-                        <span
-                          key={time}
-                          onClick={() => {
-                            props.handleSchedule(
-                              time,
-                              item.id_schedule,
-                              item.price,
-                              item.teater_name
-                            );
-                          }}
-                        >
-                          {time}
-                        </span>
-                      ))}
-                    </div>
-                    <div className="schedule-price d-flex justify-content-between pt-4">
-                      <div className="price-title">Price</div>
-                      <div className="price-amount">{item.price} / seat</div>
-                    </div>
-                    <div className="booking-button pt-4">
-                      <button className="btn-booking" onClick={props.order}>
-                        Book now
-                      </button>
-                    </div>
-                  </div>
+                  </>
                 ))
               ) : (
                 <h1 className="m-0">no data schedule</h1>
               )}
+            </div>
+            <div className="pagination-nav mt-5 d-flex justify-content-center">
+              {" "}
+              <Pagination
+                previousLabel={false}
+                nextLabel={false}
+                breakLabel={"..."}
+                pageCount={paginate.totalPage}
+                onPageChange={handlePagination}
+                containerClassName={"pagination"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                disabledClassName={"disabled"}
+                activeClassName={"active"}
+              />
             </div>
             {/* <div className="view-more text-center pt-5 my-5">
                     <span>View more</span>
